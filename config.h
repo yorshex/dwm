7523b0c2952607e54acfc27579117078d62c48df
yorshex <maxyorsh1324@gmail.com>
2024-05-18 02:41:20 +0500

    Add 'Symbols Nerd Font' to fonts

--- config.h
+++ config.h
@@ -11,7 +11,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka:size=10" };
+static const char *fonts[]          = { "Iosevka:size=10", "Symbols Nerd Font:size=10" };
 static const char dmenufont[]       = "Iosevka:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
